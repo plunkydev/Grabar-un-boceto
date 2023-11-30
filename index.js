3b8651f54892ed6660c93ee1b8de35c34863898a
plunkydev <137208896+plunkydev@users.noreply.github.com.>
2023-11-29 23:42:18 -0400

    Creadas las funciones setupGrid y gridLength,  setupGrid carga por defecto el lienso, y gridLength condigura la cantidad de grids en el lienzo

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 //apuntar a inputs
 const pixeles = document.querySelector("#pixeles");
 const lienzo = document.querySelector(".lienzo-continer");
-let length = Number(pixeles.value);
+const main = document.querySelector(".main-continer");
+
 
 function setupGrid(size) {
     lienzo.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     lienzo.style.gridTemplateRows = `repeat(${size}, 1fr)`
     for (let i = 0; i < size * size; i++) {
-    const gridElement = document.createElement('div')
-    gridElement.classList.add('pixelStyle')
-    lienzo.appendChild(gridElement)
+        const gridElement = document.createElement('div');    
+        gridElement.classList.add('pixelStyle')
+        lienzo.appendChild(gridElement)
     }
 }
+
+const gridLength = (size) => {
+    size = pixeles.value;
+    let padre = document.getElementById('idLienzo');
+    while (padre.firstChild) {
+    padre.removeChild(padre.firstChild);
+    }
+    setupGrid(size)
+};
+
+pixeles.addEventListener('change', gridLength)
 window.onload = () => {
     setupGrid(16)
     
